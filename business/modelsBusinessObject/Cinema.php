42d46cc63c7a09ba4d5a0e63db5e2bfdfca863d3
geronimoyatel <56684695+geronimoyatel@users.noreply.github.com>
2020-10-06 11:27:25 -0300

    Se agrego clase cinema y nuevas carpetas

---
+++ business/modelsBusinessObject/Cinema.php
@@ -0,0 +1,42 @@
+<?php 
+namespace business;
+
+class Cinema{
+	
+	private $capacity;
+	private $name;
+	private $address;
+	
+
+	function __construct($capacity, $name, $address){
+		$this->capacity = $capacity;
+		$this->name = $name;
+		$this->address = $address;
+    }
+    
+    public function getCapacity(){
+		return $this->capacity;
+	}
+
+	public function setCapacity($capacity){
+		$this->capacity = $capacity;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getAddress(){
+		return $this->address;
+	}
+
+	public function setAddress($address){
+		$this->address = $address;
+	}
+	
+}
+ ?>
