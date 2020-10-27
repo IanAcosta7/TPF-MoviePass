01d40eb603bbd76f34672261eea8477ddc80b93d
IanAcosta7 <ianacosta700@gmail.com>
2020-10-27 12:12:06 -0300

    db message changed

--- DAO/Database.php
+++ DAO/Database.php
@@ -5,10 +5,8 @@ class Database {
         $conn = mysqli_connect(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
         if (!$conn) {
-            die("Database Connection failed: ");
+            die("Database Connection failed:" . mysqli_connect_error());
         }
-
-        echo "Connected successfully";
     }
 }
 
