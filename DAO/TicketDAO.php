6efa300944f78f8d280f3aa5ce77e389877b7d19
JuliSchaab2001 <jupobatero1@gmail.com>
2020-11-16 22:58:49 -0300

    AHI TENES EL TICKET DAO IAN!!!!!!

---
+++ DAO/TicketDAO.php
@@ -0,0 +1,43 @@
+<?php namespace DAO;
+
+use business\models\Payment;
+
+use DAO\Database;
+
+require_once("./config/ENV.php");
+
+
+class ticketDAO {
+    private $ticket = array();
+
+    public function GetAll(){
+        $this->ticket = $this->getDBTicket();
+        return $this->ticket;
+    }
+
+    public function add($ticket){
+        Database::connect();
+        Database::execute('add_ticket', 'IN', array($ticket->getId_purchase(), $ticket->getId_show(), $ticket->getNumber(), $ticket->getQr()));
+    }
+
+    private function getDBTicket(){
+        Database::connect();
+        $DBTicket = Database::execute('get_ticket', 'OUT');
+        $DBTicket = array_map(function ($ticket){
+            $ticket = Database::execute('get_ticket_by_id', 'OUT', array($ticket['id_ticket']))[0];
+            return new Ticket(
+                $ticket["id_ticket"],
+                $ticket["id_show"],
+                $ticket["id_purchase"],
+                $ticket["ticket_number"],
+                $ticket["qr"]
+            );
+        }, $DBTicket);
+
+        return $DBTicket;
+    }
+
+   
+}
+
+?>
